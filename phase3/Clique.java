f06a3a2a9c80c44e8507f2073441e4b578bb32ee
gorilskij <43991400@users.noreply.github.com>
2019-01-09 14:04:31 +0100

    added clique possible check to clique

--- phase3/Clique.java
+++ phase3/Clique.java
@@ -6,11 +6,37 @@ import java.util.ArrayList;
 public class Clique implements Algorithm {
     public void run(Runner runner, Graph graph) {
         if (any2(graph)) runner.lowerBound(2);
+        
+        // use timeout
+        // start from lower bound, no point checking below
+        int startSize = Math.max(2, runner.currentLowerBound);
+        for (int size = startSize;; size++) {
+            if (!cliquePossible(graph, size)) break;
+            // if cliqueExists, lowerBound, else break
+        }
     }
     
     private static boolean any2(Graph graph) {
+        // checks if there are 2 nodes connected anywhere in the graph
+        // if there are -> lower bound = 2
         for (Node node : graph.nodes)
             if (!node.neighbors.isEmpty()) return true;
+        
+        return false;
+    }
+    
+    private static boolean cliquePossible(Graph graph, int size) {
+        // checks, based on numbers of neighbors, whether it's possible
+        // to find a clique of the given size
+        int minNeighbors = size - 1;
+        int nodesFound = 0;
+        
+        for (Node node : graph.nodes) {
+            if (node.neighbors.size() >= minNeighbors) {
+                nodesFound++;
+                if (nodesFound >= size) return true;
+            }
+        }
         
         return false;
     }
