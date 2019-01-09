52829484c1b0487b9f2bb6ac3b22b5b3c8fad110
gorilskij <43991400@users.noreply.github.com>
2019-01-09 14:15:03 +0100

    finished brooks' theorem

--- phase3/BrooksTheorem.java
+++ phase3/BrooksTheorem.java
@@ -2,7 +2,7 @@ package phase3;
 
 import java.io.IOException;
 
-// NOT FINISHED
+// FINISHED, NOT CHECKED
 public class BrooksTheorem implements Algorithm {
     public void run(Runner runner, Graph graph) {
         int maxNeighbors = 0;
@@ -16,6 +16,30 @@ public class BrooksTheorem implements Algorithm {
         // delta + 1 is upper bound
         runner.upperBound(maxNeighbors + 1);
         
-        // check if delta is upper bound
+        if (isCycle(graph)) {
+            // if graph is odd cycle, delta + 1 is the upper bound
+            if (graph.nodes.size() % 2 == 1) return; // stop checking
+        }
+        
+        if (isComplete(graph)) return; // stop checking
+        
+        // at this point the graph is not complete and not an odd cycle
+        // so the actual upper bound is just delta
+        runner.upperBound(maxNeighbors);
+    }
+    
+    private static boolean isComplete(Graph graph) {
+        // check if the graph is a clique
+        int n = graph.nodes.size();
+        return graph.numberOfEdges == (n * (n - 1)) / 2;
+    }
+    
+    private static boolean isCycle(Graph graph) {
+        // for a graph to be a cycle, it must be a
+        // single circle with 2 edges per node
+        for (Node node : graph.nodes)
+            if (node.neighbors.size() != 2) return false;
+        
+        return true;
     }
 }
