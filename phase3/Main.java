f9abb6504ae24628ac659d5b5983c0942e178fd5
gorilskij <43991400@users.noreply.github.com>
2019-01-15 16:04:26 +0100

    updated / fixed compile_and_run_win
    bonus: now you can run it from any directory and it will cd to its own directory automatically

--- phase3/Main.java
+++ phase3/Main.java
@@ -12,30 +12,30 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         // this is where the program starts
-        
+
         Graph graph = Reader.readGraph("phase3/graphs_phase1/graph20.txt");
-        
+
         // prints out how many separate graphs there are
         // System.out.println(graphs.size() + " section in graph");
-        
+
         // TODO: check the ordering of the algorithms (fastest first)
-        // all usable algorithms
+        // all usable algorithms (commas with nothing after them cause an error)
         ArrayList<Class<? extends Algorithm>> algorithms = new ArrayList<Class<? extends Algorithm>>(
             Arrays.asList(
                 // BrooksTheorem.class,
                 // Tree.class,
                 // Clique.class,
-                // StephansAlgorithm.class,
+                StephansAlgorithm.class
                 // AdjacencyMatrix.class,
                 // BasicExact.class,
                 // WelshPowell.class,
-                
+
                 // these are work in progress, don't use them
                 // PrunedExact.class,
                 // HoffmanBound.class
             )
         );
-        
+
         SuperRunner runner = new SuperRunner(graph, algorithms);
     }
 }
